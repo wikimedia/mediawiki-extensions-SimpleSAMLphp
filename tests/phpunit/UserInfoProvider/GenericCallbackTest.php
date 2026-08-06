98722076829d768d25cc7e2df91807b0f113d27c
Hamish <hamish.slater@hallowelt.com>
2026-08-06 21:35:41 +0700

    Update HashConfig namespace
    ERM48053

--- tests/phpunit/UserInfoProvider/GenericCallbackTest.php
+++ tests/phpunit/UserInfoProvider/GenericCallbackTest.php
@@ -3,7 +3,7 @@
 namespace MediaWiki\Extension\SimpleSAMLphp\Tests\UserInfoProvider;
 
 use Exception;
-use HashConfig;
+use MediaWiki\Config\HashConfig;
 use MediaWiki\Extension\SimpleSAMLphp\UserInfoProvider\GenericCallback;
 use PHPUnit\Framework\TestCase;
 
